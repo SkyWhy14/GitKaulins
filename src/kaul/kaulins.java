7eef8087a2ef3d0bf1e47ef86c43df969844a847
Sky <artursgravitis312@gmail.com>
2025-09-02 22:07:35 +0300

    Iespejams ievadit metamu skaitu no konsoles

--- src/kaul/kaulins.java
+++ src/kaul/kaulins.java
@@ -1,15 +1,23 @@
 package kaul;
 
 import java.util.Random;
+import java.util.Scanner;
+
+
 
 public class kaulins {
 
 	public static void main(String[] args) {
-		int skaitlis;
+		int skaitlis,reizes;
 		Random rand = new Random();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kaulinu");
+		reizes = scan.nextInt();
+		for(int i=1;i<=reizes;i++) {
+		
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis "+skaitlis);
-
+		}
 	}
 
 }
