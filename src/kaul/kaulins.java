66eca51d115bd30ba73bfd0833feaa9c9cbaf6d2
Sky <artursgravitis312@gmail.com>
2025-09-02 21:57:59 +0300

    Papildinats ar funkcionalitati ka metamo kaulinu var mest vairakas reizes

--- src/kaul/kaulins.java
+++ src/kaul/kaulins.java
@@ -4,15 +4,16 @@ import java.util.Random;
 
 public class kaulins {
 
-	static void mestkaulinu() {
+	static void mestkaulinu(int reizes) {
 		int skaitlis;
 		Random rand = new Random();
+		for(int i=1;i<=reizes;i++) {
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis "+skaitlis);
-
+		}
 	}
 	public static void main(String[] args) {
-		mestkaulinu();
+		mestkaulinu(3);
 	}
 }
 	
