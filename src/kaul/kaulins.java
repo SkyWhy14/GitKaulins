4331121e6b7812ca89e5e93db0dc8f374739a9b7
Sky <artursgravitis312@gmail.com>
2025-09-02 21:55:59 +0300

    Refakrotrets kods un ieviesta metode

--- src/kaul/kaulins.java
+++ src/kaul/kaulins.java
@@ -4,12 +4,18 @@ import java.util.Random;
 
 public class kaulins {
 
-	public static void main(String[] args) {
+	static void mestkaulinu() {
 		int skaitlis;
 		Random rand = new Random();
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis "+skaitlis);
 
 	}
-
+	public static void main(String[] args) {
+		mestkaulinu();
+	}
 }
+	
+		
+	
+
